ba673712fbcb762c46191ba9b06c896cce3b4be0
Vuong Dang <ddvuong@gmail.com>
2021-01-07 05:08:29 +0700

    Add Head element to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import {AppProps} from 'next/app'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NextJS TailwindCSS TypeScript Starter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
